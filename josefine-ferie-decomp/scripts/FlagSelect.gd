9f6e4c42dfc3cc28f4a5addd307edbabde71c7bb
John Olav Haraldstad <john.haraldstad@hotmail.com>
2026-05-09 20:00:48 +0200

    Click sound

--- josefine-ferie-decomp/scripts/FlagSelect.gd
+++ josefine-ferie-decomp/scripts/FlagSelect.gd
@@ -1,19 +1,24 @@
 extends Node2D # or Sprite2D, depending on your root node type
 
-func _on_norway_flag_pressed():
-	# 1. Tell Godot to use Norwegian translations
-	TranslationServer.set_locale("no")
+@onready var sfx_player = $SfxPlayer # Or just $AudioStreamPlayer
+
+func change_screen():
+	# 1. Play the sound
+	sfx_player.play()
 	
-	# 2. (Optional) Play a sound if you have one
-	# $AudioPlayer.stream = load("res://assets/audio/welcome_no.wav")
-	# $AudioPlayer.play()
-	
-	# 3. Change to the forest scene
+	# 2. Wait for the sound to finish before moving on
+	# This 'await' tells the script: "Pause here until the signal 'finished' happens"
+	await sfx_player.finished
 	get_tree().change_scene_to_file("res://scenes/picnic.tscn")
 
+func _on_norway_flag_pressed():
+	# Tell Godot to use Norwegian translations
+	TranslationServer.set_locale("no")
+	
+	change_screen()
+
 func _on_sweden_flag_pressed():
-	# 1. Tell Godot to use Swedish translations
+	# Tell Godot to use Swedish translations
 	TranslationServer.set_locale("sv")
 	
-	# 2. Change to the forest scene
-	get_tree().change_scene_to_file("res://scenes/picnic.tscn")
+	change_screen()
